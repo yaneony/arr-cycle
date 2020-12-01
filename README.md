5ff43984317257759e493c1593b383cf37eff2e7
Jan Sieler <jan@sieler.im>
2020-12-01 10:52:58 +0100

    All current changes.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # arr-cycle
+> Cycle thru all items in an array.
+
+## Install
+```
+npm install arr-cycle
+```
+
+## Usage
+```js
+const Cycle = require('arr-cycle');
+
+var items = new Cycle(['foo', 'bar', 'unicorn']);
+
+items.curr();
+items.prev();
+items.next();
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+class Cycle {
+  constructor(list) {
+    this._list = list || [];
+    this._index = undefined;
+  };
+
+  curr() {
+    return this._list[this._index];
+  };
+
+  prev() {
+    if (this._list[this._index] == undefined) {
+      this._index = this._list.length - 1;
+    }
+    this._item = this._list[this._index];
+    this._index--;
+    return this._item;
+  };
+
+  next() {
+    if (this._list[this._index] == undefined) {
+      this._index = 0;
+    }
+    this._item = this._list[this._index];
+    this._index++;
+    return this._item;
+  };
+}
+
+module.exports = Cycle;
